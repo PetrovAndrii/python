644d043ad7c3e2bdf4f9ea89db16b194f57eb44d
PetrovAndrii <andrey9314@gmail.com>
2017-06-15 11:05:54 +0300

    Добавлен новый тест на проверку контактов

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 class Application:
 
@@ -10,6 +11,7 @@ class Application:
         self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
 
     def is_valid(self):
@@ -21,7 +23,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
