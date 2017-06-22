4ca5b661916e31bbb79adbf09d041f7d66548c20
PetrovAndrii <andrey9314@gmail.com>
2017-06-22 08:37:52 +0300

    Загрузка данных из файла

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from model.group import  Group
+from model.group import Group
 import pytest
-import random
-import string
+from data.add_group import constant as testdata
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return  prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [ Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
-]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
@@ -24,11 +13,3 @@ def test_add_group(app, group):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-#testdata = [
-#    Group(name=name, header=header, footer=footer),
-#    for name in ["", random_string("name", 10)]
-#    for header in ["", random_string("header", 20)]
-#    for footer in ["", random_string("footer", 20)]
-#    ]
